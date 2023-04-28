e56034c3885faad2c2ae362d15a5669c4a8d412e
NathanWarrick <nathan@wimmerait.com.au>
2023-04-28 20:06:21 +1000

    papercut bot fully functioning

--- workspace/bots/papercut_bot/papercut_bot.py
+++ workspace/bots/papercut_bot/papercut_bot.py
@@ -5,17 +5,14 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 import os
-import pyautogui
 import time
 
 from workspace.bots import secret
-
 
 # --------------------------SETUP-------------------------- #
 dir_path = os.path.dirname(os.path.realpath(__file__))
 downloads = dir_path + r"\downloads" # Create downloads path for files to be downloaded using selenium
 driverlocation = r'workspace\assets\drivers\chromedriver.exe'
-
 
 view_button_array = []
 
@@ -65,46 +62,8 @@ class Browser: # Selenium Browser Configuration
         self.add_input(by=By.NAME, value='creditAmount', text=amount)
         self.add_input(by=By.NAME, value='paymentMethod', text=paymentmethod) # EFT, Cash or Other
         self.add_input(by=By.NAME, value='$TextField', text=comment)
-        self.click_button(by=By.NAME, value='$Submit')
-        #self.click_button(by=By.NAME, value='$Submit$0')
+        self.click_button(by=By.NAME, value='$Submit$0')
         
-class Browser_Headless: # Selenium Browser Configuration
-    browser, service = None, None
-
-    # Initialise the webdriver with the path to chromedriver.exe
-    def __init__(self, driver: str):
-        self.service = Service(driver)
-        
-        options = Options()
-        #options.add_argument("start-maximized")
-        options.headless = True
-        options.add_argument('window-size=1920x1080');
-        options.add_experimental_option("prefs", {"download.default_directory": downloads})
-        
-        
-        self.browser = webdriver.Chrome(service=self.service, chrome_options=options)
-    
-    def open_page(self, url: str):
-        self.browser.get(url)
-
-    def close_browser(self):
-        self.browser.close()
-        
-    def add_input(self, by: By, value: str, text: str):
-        field = self.browser.find_element(by=by, value=value)
-        field.send_keys(text)
-        time.sleep(1)
-        
-    def click_button(self, by: By, value: str):
-        button = self.browser.find_element(by=by, value=value)
-        button.click()
-        time.sleep(1)
-        
-    def login_papercut(self, username: str, password: str):
-        self.add_input(by=By.NAME, value='inputUsername', text=username)
-        self.add_input(by=By.NAME, value='inputPassword', text=password)
-        self.click_button(by=By.CLASS_NAME, value='loginSubmit')
-
 
 def deposit(ref, amount, method, comment):   
     
@@ -120,6 +79,4 @@ def deposit(ref, amount, method, comment):
         browser.login_papercut(secret.papercut_username, secret.papercut_password)
     browser.open_page('https://papercut.horsham-college.vic.edu.au/app?service=page/WebCashierDeposit')
     browser.papercut_deposit(ref, amount, method, comment)
-    time.sleep(1)
     
-
