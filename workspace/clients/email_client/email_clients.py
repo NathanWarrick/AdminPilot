ed4533c4981038226cdac2df1c83ba7a8473aaf9
NathanWarrick <nathan@wimmerait.com.au>
2023-05-16 10:39:27 +1000

    added camp_check and qkr interactions

--- workspace/clients/email_client/email_clients.py
+++ workspace/clients/email_client/email_clients.py
@@ -16,6 +16,8 @@ driverlocation = r'workspace\assets\drivers\chromedriver.exe'
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 inbox = outlook.GetDefaultFolder(6) # "6" refers to the index of a folder - in this case the Inbox
 
+qkrfolder = {'Jan': '1. January','Feb': '2. February','Mar': '3. March','Apr': '4. April','May': '5. May','Jun': '6. June','July': '7. Jul','Aug': '8. August','Sep': '9. September','Oct': '10. October','Nov': '11. November','Dec': '12. Decemmber'}
+
 folders = {}    
 
 class Browser: # Selenium Browser Configuration
@@ -130,6 +132,112 @@ def excursion_check():
                     i = i + 1  # Try the next attachment
                     
             os.remove(os.getcwd() + r'\workspace\clients\email_client\downloads' + '\\' + attachment.FileName) # Remove file upon completion
+            
+def camp_check():
+    browser = Browser(driverlocation)
+    for msg in inbox.Folders(folders['1 Excursions']).Items:
+        if re.match('Camp Approved - .+', msg.Subject) and msg.FlagStatus == 2: # Grab flagged excursion approvals
+            subject = msg.Subject
+            #print('Subject: ' + subject)
 
+            match = 0
+            i = 1
+            while match == 0:
+                if not re.search('Medical Form', str(msg.Attachments.Item(i))) and not re.search('.png', str(msg.Attachments.Item(i))): # Check that the attachment doesn't contain the terms medical form or .png in the title
+                    attachment = msg.Attachments.Item(i)
+                    #print('Path: ' + os.getcwd() + r'\workspace\clients\email_client\downloads' + '\\' + attachment.FileName)
+                    attachment.SaveAsFile(os.getcwd() + r'\workspace\clients\email_client\downloads' + '\\' + attachment.FileName)
+                    match = 1
+                    
+                    # Read the PDF
+                    form = PyPDF2.PdfReader(os.getcwd() + r'\workspace\clients\email_client\downloads' + '\\' + attachment.FileName)
+                    data = form.pages[0].extract_text()
+                    #print(data)
+                    
+                    # ************************************************ Data Extraction ************************************************
+                    
+                    # Excursion Name
+                    excursionnamestart = subject.find('Camp Approved')
+                    excursionnameend = subject.find('\n')
+                    excursionname = subject[excursionnamestart+21:]
+                    print('Name: ' + excursionname)               
+                    
+                    # Excursion Date
+                    dateloc = data.find('Date /s')
+                    datenl = data.find('\n', dateloc)
+                    date = data[dateloc+8:datenl]
+                    print('Date: ' + date)
+                    
+                    # Excursion Cost
+                    costloc = data.find('Details of Cost')
+                    costnl = data.find('\n', costloc)   
+                    cost = data[costloc+16:costnl]     
+                    print('Cost: ' + cost)
+                    print('\n')
+                    
+                    # ************************************************ Begin QKR ************************************************
+                    
+                    browser.open_page('https://qkr-mss.qkrschool.com/qkr_mss/index.html')
+                    time.sleep(2)
+                    browser.login_qkr('nathan.warrick@education.vic.gov.au', 'B@yed133')
+                    time.sleep(1)
+                    browser.open_page('https://qkr-mss.qkrschool.com/qkr_mss/app/storeFront#/inventory')
+                    time.sleep(2)
+                    browser.click_button(by=By.XPATH, value='//*[@id="addProduct"]/span')
+                    time.sleep(1)
+                    browser.add_input(by=By.XPATH, value='//*[@id="productname"]', text=excursionname)
+                    time.sleep(.1)
+                    browser.add_input(by=By.XPATH, value='//*[@id="displaycategoryName1"]', text='Camps')
+                    time.sleep(.1)
+                    browser.add_input(by=By.XPATH, value='//*[@id="shortDescription"]', text=date)
+                    time.sleep(.1)
+                    browser.add_input(by=By.XPATH, value='//*[@id="amount"]', text=cost)
+                    time.sleep(.1)
+                    browser.click_button(by=By.XPATH, value='//*[@id="saveBtn"]')
+                else:
+                    i = i + 1  # Try the next attachment
+                    
+            os.remove(os.getcwd() + r'\workspace\clients\email_client\downloads' + '\\' + attachment.FileName) # Remove file upon completion
+            
+def qkr_cases_report():
+    for msg in inbox.Folders(folders['QKR To Process']).Items:
+        if re.match('Qkr! Accounting System.+', msg.Subject) and msg.FlagStatus == 2: # Grab flagged excursion approvals
+            subject = msg.Subject
+            #print('Subject: ' + subject)
 
-excursion_check()
+            match = 0
+            i = 1
+            while match == 0:
+                if re.search('.csv', str(msg.Attachments.Item(i))): # Check if the file is a csv
+                    attachment = msg.Attachments.Item(i)
+                    month = qkrfolder.get(attachment.FileName[21:24]) # Extract month from attachment name and reference with dictionary for file path translation
+                    year = attachment.FileName[17:21] # Extract year from attachment name
+                    attachment.SaveAsFile(r'U:\PUBLIC\Finance\.QKR Files' + '\\' + year + '\\' + month + '\\' + attachment.FileName) # Save .csv to the U drive
+                    #return(r'U:\PUBLIC\Finance\.QKR Files' + '\\' + year + '\\' + month + '\\' + attachment.FileName)
+                    match = 1
+                    print(r'U:\PUBLIC\Finance\.QKR Files' + '\\' + year + '\\' + month + '\\' + attachment.FileName)
+                          
+                else:
+                    i = i + 1  # Try the next attachment
+                            
+def qkr_transaction_report():
+    for msg in inbox.Folders(folders['QKR To Process']).Items:
+        if re.match('Qkr! Report Transaction Details Report', msg.Subject) and msg.FlagStatus == 2: # Grab flagged excursion approvals
+            subject = msg.Subject
+            print('Subject: ' + subject)
+
+            match = 0
+            i = 1
+            while match == 0:
+                if re.search('.xls', str(msg.Attachments.Item(i))): # Check if the file is a csv
+                    attachment = msg.Attachments.Item(i)
+                    #print('Path: ' + os.getcwd() + r'\workspace\clients\email_client\downloads' + '\\' + attachment.FileName)
+                    attachment.SaveAsFile(os.getcwd() + r'\workspace\clients\email_client\downloads' + '\\' + attachment.FileName)
+                    match = 1           
+                else:
+                    i = i + 1  # Try the next attachment
+                    
+            #os.remove(os.getcwd() + r'\workspace\clients\email_client\downloads' + '\\' + attachment.FileName) # Remove file upon completion
+            
+print(qkr_cases_report())
+#qkr_transaction_report()
