4a54cc586a487f79066837dc18b5af7929eec7b9
NathanWarrick <nathan@wimmerait.com.au>
2023-09-06 21:56:11 +1000

    Enforce PEP8 Compliance

--- workspace/clients/openai/whisper/transcription_test.py
+++ workspace/clients/openai/whisper/transcription_test.py
@@ -2,10 +2,12 @@ import whisper
 import openai
 import requests
 
-openai.api_key = 'API Here'
+openai.api_key = "API Here"
 
 model = whisper.load_model("tiny.en")
-result = model.transcribe(r'workspace\clients\openai\whisper\test_files\transcribing_1.mp3')
+result = model.transcribe(
+    r"workspace\clients\openai\whisper\test_files\transcribing_1.mp3"
+)
 response = result["text"]
 print(response)
 
@@ -20,16 +22,21 @@ print(response)
 # print("==========================================================================================================================================================+==========")
 # for choice in summary['choices']:
 #     print(choice['text'])
-    
+
 
 API_URL = "https://api-inference.huggingface.co/models/nomic-ai/gpt4all-j"
 headers = {"Authorization": "Bearer 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"}
 
+
 def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
-	
-output = query({
-	"inputs": "Summarise the following transription in a form that would be suitable for a councils minutes, complete with enough detail that somoene that didn't attend the meeting would understand everything that happened:" + response,
-})
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+
+output = query(
+    {
+        "inputs": "Summarise the following transription in a form that would be suitable for a councils minutes, complete with enough detail that somoene that didn't attend the meeting would understand everything that happened:"
+        + response,
+    }
+)
 print(output)
--- workspace/data/functionsadvanced.py
+++ workspace/data/functionsadvanced.py
@@ -6,12 +6,13 @@ import pyautogui
 import pytesseract
 import win32gui
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract"
+
 
 class WindowMgr:
     """Encapsulates some calls to the winapi for window management"""
 
-    def __init__ (self):
+    def __init__(self):
         """Constructor"""
         self._handle = None
 
@@ -32,46 +33,55 @@ class WindowMgr:
     def set_foreground(self):
         """put the window in the foreground"""
         win32gui.SetForegroundWindow(self._handle)
-        
+
+
 def batch_report():
     try:
-        Batch_Location = pyautogui.locateCenterOnScreen(r'workspace/assets/general/Batch_Number.png')
+        Batch_Location = pyautogui.locateCenterOnScreen(
+            r"workspace/assets/general/Batch_Number.png"
+        )
         Batch_Location_X = Batch_Location[0]
         Batch_Location_Y = Batch_Location[1]
     except:
-        return("Can't find batch number")
-    im = pyautogui.screenshot(region=((Batch_Location_X+45), (Batch_Location_Y-10), 40, 15),)
-    im.save(r'workspace/assets/temp/batch.png')
+        return "Can't find batch number"
+    im = pyautogui.screenshot(
+        region=((Batch_Location_X + 45), (Batch_Location_Y - 10), 40, 15),
+    )
+    im.save(r"workspace/assets/temp/batch.png")
 
-    img = cv2.imread(r'workspace/assets/temp/batch.png')
+    img = cv2.imread(r"workspace/assets/temp/batch.png")
     img = cv2.resize(img, None, fx=2, fy=2)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        
-    config = '--oem 3 --psm 6' 
-    batch = pytesseract.image_to_string(img, config = config, lang='eng')
+    config = "--oem 3 --psm 6"
+    batch = pytesseract.image_to_string(img, config=config, lang="eng")
     batch = batch[:5]
     return batch
 
+
 def reference_report():
     try:
-        Batch_Location = pyautogui.locateCenterOnScreen(r'workspace/assets/general/Reference.png')
+        Batch_Location = pyautogui.locateCenterOnScreen(
+            r"workspace/assets/general/Reference.png"
+        )
         Batch_Location_X = Batch_Location[0]
         Batch_Location_Y = Batch_Location[1]
     except:
-        return("Cant Find reference number")
-    im = pyautogui.screenshot(region=((Batch_Location_X+38), (Batch_Location_Y-10), 65, 15),)
-    im.save(r'workspace/assets/temp/reference.png')
+        return "Cant Find reference number"
+    im = pyautogui.screenshot(
+        region=((Batch_Location_X + 38), (Batch_Location_Y - 10), 65, 15),
+    )
+    im.save(r"workspace/assets/temp/reference.png")
 
-    img = cv2.imread(r'workspace/assets/temp/reference.png')
+    img = cv2.imread(r"workspace/assets/temp/reference.png")
     img = cv2.resize(img, None, fx=2, fy=2)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        
-    config = '--oem 3 --psm 6' 
-    reference = pytesseract.image_to_string(img, config = config, lang='eng')
+    config = "--oem 3 --psm 6"
+    reference = pytesseract.image_to_string(img, config=config, lang="eng")
     reference = reference[:10]
     return reference
+
 
 def focus(focus_on):
     try:
